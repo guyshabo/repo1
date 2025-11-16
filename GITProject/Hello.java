0597685dc6aae6861b84ce0a534bce8005cc8ddd
guyshabo <guyshabog@gmail.com>
2025-11-16 20:14:29 +0200

    code change

--- GITProject/Hello.java
+++ GITProject/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
+        int g=6;
         for (int i = 0; i <=10; i++) {
             System.out.println("Hello World..." + i);
         }
