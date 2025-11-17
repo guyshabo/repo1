a06aacc03d3f4d2b529e65a044565604337d2f92
guyshabo <guyshabog@gmail.com>
2025-11-17 12:17:23 +0200

    Update GuyShabo.java with new print

--- GuyShabo.java
+++ GuyShabo.java
@@ -1,6 +1,6 @@
 public class GuyShabo {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.println("Guy Shabo");
         }
     }
